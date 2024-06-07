3fbb6ea636338427dcd39e0162f5cc43e852a73f
unnisama <gola.lola17@gmail.com>
2024-06-07 13:26:45 +0530

    done

--- client.c
+++ client.c
@@ -1,27 +1,42 @@
 #include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define MAX_EVENTS 10
+
+int epoll_fd;
+struct epoll_event ev, events[MAX_EVENTS];
+
+int port;
+char is_first = 1;
 
 int main(int argc, char** argv)
 {
 
-    if (argc < 2) {
-        printf("Usage: %s <port>\n", argv[0]);
+    if (argc < 3) {
+        printf("Usage: %s <ip> <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    int port = atoi(argv[1]);
+    port = atoi(argv[2]);
+
+    fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
     printf("%d\n", port);
     char buffer[1024];
     int client_fd;
     struct sockaddr_in serv_addr;
 
+    epoll_fd = epoll_create1(0);
+
     serv_addr.sin_port = htons(port);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_family = AF_INET;
 
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -39,26 +54,94 @@ int main(int argc, char** argv)
 
     printf("[+] Connected successfully \n");
 
+    ev.data.fd = client_fd;
+    ev.events = EPOLLIN | EPOLLHUP | EPOLLRDHUP;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &ev) < 0) {
+        shutdown(client_fd, SHUT_WR);
+        perror("epoll_ctl");
+        close(epoll_fd);
+        exit(EXIT_FAILURE);
+    }
+    int stdin_fileno = fileno(stdin);
+    ev.data.fd = stdin_fileno;
+    ev.events = EPOLLIN | EPOLLHUP | EPOLLRDHUP;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, ev.data.fd, &ev) < 0) {
+        shutdown(client_fd, SHUT_WR);
+        perror("epoll_ctl");
+        close(epoll_fd);
+        exit(EXIT_FAILURE);
+    }
+
     while (1) {
-        printf("You: ");
-
-        char* result = fgets(buffer, sizeof(buffer), stdin);
-
-        if (result == NULL) {
-            // Error or EOF encountered
-            break;
+        if (is_first) {
+            printf("> ");
+            fflush(stdout);
+            is_first = 0;
         }
-
-        int bytes_read = strlen(result);
-
-        if (bytes_read == 0) {
-            continue;
-        }
-        size_t bytes_send = send(client_fd, buffer, bytes_read, 0);
-        if (bytes_read != bytes_send) {
-            perror("Could't send data to server");
-            close(client_fd);
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds < 0) {
+            shutdown(client_fd, SHUT_WR);
+            perror("epoll_wait");
+            close(epoll_fd);
             exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == stdin_fileno) {
+                char buffer[1024];
+                int result;
+
+                while ((result = read(0, &buffer, 1024)) > 0) {
+                    if (strcmp(buffer, "q\n") == 0) {
+                        if (shutdown(client_fd, SHUT_WR) != -1) {
+                            printf("Shuting Downing!\n");
+                            close(epoll_fd);
+                            exit(EXIT_SUCCESS);
+                        } else {
+                            perror("shutdown");
+                            close(epoll_fd);
+                            exit(EXIT_SUCCESS);
+                        }
+                    }
+                    size_t bytes_send = send(client_fd, buffer, result, MSG_DONTWAIT);
+                    if (result != bytes_send) {
+                        perror("Could't send data to server");
+                        shutdown(client_fd, SHUT_WR);
+                        close(epoll_fd);
+                        exit(EXIT_FAILURE);
+                    }
+                    printf("> ");
+                    fflush(stdout);
+                }
+            } else if (events[i].data.fd == client_fd) {
+
+                if (events[i].events & (EPOLLHUP | EPOLLRDHUP)) {
+                    close(epoll_fd);
+                    printf("Server isn't working\n");
+                    exit(EXIT_FAILURE);
+                }
+                int bytes_recv;
+                char buffer[1024];
+                while (1) {
+                    bytes_recv = recv(client_fd, &buffer, 1024, MSG_DONTWAIT);
+                    if (bytes_recv == 0) {
+                        break;
+                    } else if (bytes_recv < 0) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            break;
+                        }
+                        perror("recv");
+                        break;
+                    }
+                    for (int i = 0; i < bytes_recv; i++) {
+                        printf("%c", buffer[i]);
+                    }
+                    printf("> ");
+                    fflush(stdout);
+                }
+            }
         }
     }
 }
--- server.c
+++ server.c
@@ -1,26 +1,47 @@
 #include <arpa/inet.h>
+#include <asm-generic/errno-base.h>
+#include <bits/types/sigset_t.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+
+#define MAX_CONNECTIONS 20
+
+#define MAX_EVENTS 10
+
+int epoll_fd;
+struct epoll_event ev, events[MAX_EVENTS];
+int server_fd, client_socket;
+
+int fds[MAX_CONNECTIONS];
+int currentfds = 0;
 
 int get_random_number(int min, int max)
 {
     return min + (rand() % (max - min));
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    srand(time(NULL));
-    int server_fd, new_socket;
+    if (argc < 3) {
+        printf("Usage: %s <ip> <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    int port = get_random_number(4000, 65000);
+    epoll_fd = epoll_create1(0);
+    srand(time(NULL));
+
+    int port = atoi(argv[2]);
 
     struct sockaddr_in addr;
     addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
     addr.sin_family = AF_INET;
     int addrlen = sizeof(addr);
 
@@ -45,40 +66,117 @@ int main()
     }
     printf("[+] Set Socket in listen mode\n");
 
+    ev.data.fd = server_fd;
+    ev.events = EPOLLIN;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1) {
+        perror("epoll_ctl");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
     while (1) {
 
         struct sockaddr_in client_addr;
         socklen_t client_addrlen = sizeof(client_addr);
 
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addrlen)) < 0) {
-            perror("Accept failed!");
+        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds < 0) {
+            close(server_fd);
+            close(epoll_fd);
+            for (int i = 0; i < currentfds; i++) {
+                shutdown(fds[i], SHUT_WR);
+            }
+            if (errno == EINTR) {
+                printf("Shuting Down!\n");
+                break;
+            }
+            perror("epoll_wait error!");
             close(server_fd);
             exit(EXIT_FAILURE);
         }
-        printf("[+] New Connection (%d)\n", new_socket);
-        int pid = fork();
-        if (pid == 0) {
-            close(server_fd);
-            printf("[+] Forked successfully\n");
-            int bytes_recv;
-            char buffer[1024];
-            while ((bytes_recv = recv(new_socket, &buffer, 1024, 0)) > 0) {
-                for (int i = 0; i < bytes_recv; i++) {
-                    printf("%c", buffer[i]);
+
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == server_fd) {
+                if ((client_socket = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addrlen)) < 0) {
+                    perror("Accept failed!");
+                    continue;
+                }
+
+                ev.data.fd = client_socket;
+                ev.events = EPOLLIN | EPOLLHUP | EPOLLRDHUP;
+
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &ev) == -1) {
+                    perror("epoll_ctl");
+                    close(server_fd);
+                    close(client_socket);
+                    exit(EXIT_FAILURE);
+                }
+
+                fds[currentfds] = client_socket;
+                currentfds += 1;
+
+                printf("[+] New Connection (%d)\n", client_socket);
+            } else {
+                client_socket = events[i].data.fd;
+                if (events[i].events & (EPOLLHUP | EPOLLRDHUP)) {
+                    printf("Client disconnected\n");
+                    ev.data.fd = client_socket;
+                    ev.events = 0;
+
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_socket, &ev) == -1) {
+                        perror("epoll_ctl");
+                        close(server_fd);
+                        close(client_socket);
+                        exit(EXIT_FAILURE);
+                    }
+
+                    for (int i = 0; i < currentfds; i++) {
+                        if (fds[i] == client_socket) {
+                            currentfds -= 1;
+                            for (int j = i; j < currentfds; j++) {
+                                fds[i] = fds[i + 1];
+                            }
+                            break;
+                        }
+                    }
+                    close(client_socket);
+                    continue;
+                }
+                int bytes_recv;
+                char buffer[1024];
+                sprintf(buffer, "[%d]", client_socket);
+                bytes_recv = strlen(buffer);
+
+                for (int i = 0; i < currentfds; i++) {
+                    if (fds[i] == client_socket) {
+                        continue;
+                    }
+                    int ret = send(fds[i], &buffer, bytes_recv, MSG_DONTWAIT);
+                }
+                printf("%s> ", buffer);
+                while (1) {
+                    bytes_recv = recv(client_socket, &buffer, 1024, MSG_DONTWAIT);
+                    if (bytes_recv == 0) {
+                        break;
+                    } else if (bytes_recv < 0) {
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            break;
+                        }
+                        perror("recv");
+                        break;
+                    }
+                    for (int i = 0; i < bytes_recv; i++) {
+                        printf("%c", buffer[i]);
+                    }
+                    for (int i = 0; i < currentfds; i++) {
+                        if (fds[i] == client_socket) {
+                            continue;
+                        }
+                        int ret = send(fds[i], &buffer, bytes_recv, MSG_DONTWAIT);
+                    }
                 }
             }
-
-            if (bytes_recv == 0) {
-                printf("Client disconnected\n");
-            } else if (bytes_recv < 0) {
-                perror("recv");
-            }
-
-        } else if (pid > 0) {
-            close(new_socket);
-        } else {
-            perror("fork failed");
-            close(new_socket);
         }
     }
 }
